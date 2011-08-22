ab67179659eefc957bf3a0ab3492253992208f27
Santiago Coffey <santiago.coffey@gmail.com>
2011-08-22 16:27:09 -0300

    Importing main raytracer.jar file and run scripts

---
+++ raytracer.bat
@@ -0,0 +1,2 @@
+
+@java -Djava.library.path=lib -jar raytracer.jar %1 %2 %3 %4 %5 %6 %7 %8 %9
---
+++ raytracer.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+java -Djava.library.path=lib -jar raytracer.jar $1 $2 $3 $4 $5 $6 $7 $8 $9
+
